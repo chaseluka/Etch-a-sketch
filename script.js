c816d53702081093d73b3f7917142397b013d3dc
Chase Lukasiewicz <chaseluka@gmail.com>
2022-03-15 13:49:00 -0600

    index.html

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 const grid = document.querySelector('.grid-container');
+const btnClear = document.getElementById('clear');
+const btnEraser = document.getElementById('eraser');
+const btnDraw = document.getElementById('draw');
+let clickCount = 0;
 
 function div() {
     const div = document.createElement('div');
@@ -11,9 +15,47 @@ for (i = 0; i < 256; ++i) {
     div();
 };
 
+function draw(e){
+    ++clickCount;
+   
+    grid.addEventListener("mouseover", function(e){
+        e.target.style.backgroundColor = '#000';
+    }, false);
 
+}; 
 
+function eraser(e){
+    grid.addEventListener("mouseover", function(e){
+        e.target.style.backgroundColor = '#fff';
+    }, false);
 
-grid.addEventListener("mouseover", function(e) {
-   e.target.style.backgroundColor = '#000';
-  }, false);
+    if (grid.removeEventListener('click', eraser, false));
+};
+
+function utensil(e){
+
+    if (btnDraw.addEventListener('click', function(e) {
+        if (grid.addEventListener('click', function(){
+            draw();
+            
+            if (clickCount > 1){
+                grid.removeEventListener('click', draw)
+            };
+            
+        }));
+    }));
+
+    else if (btnEraser.addEventListener('click', function(e) {
+        if (grid.addEventListener('click', function(){
+            eraser();
+        }));
+    }));
+};
+ 
+utensil();
+
+function clear(){
+    grid.style.backgroundColor = '#fff'
+};
+
+btnClear.onclick = clear;
