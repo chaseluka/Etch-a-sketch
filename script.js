b2645f1611ddd4aa8879cd359b9be93f81af9718
Chase Lukasiewicz <chaseluka@gmail.com>
2022-03-16 15:09:02 -0600

    script.js

--- script.js
+++ script.js
@@ -3,59 +3,72 @@ const grid = document.querySelector('.grid-container');
 const btnClear = document.getElementById('clear');
 const btnEraser = document.getElementById('eraser');
 const btnDraw = document.getElementById('draw');
-let clickCount = 0;
+const slider = document.querySelector('.slider');
 
-function div() {
+let clickCount = 0;
+let n = slider.value;
+
+//Grid creation/adjustment
+function gridLayout(){
+    grid.style.gridTemplateColumns = `repeat(${n}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${n}, auto)`;
+
+};
+gridLayout();
+
+function createDiv() {
     const div = document.createElement('div');
     grid.appendChild(div);
     div.classList.add('item');
+    
 };
 
-for (i = 0; i < 256; ++i) {
-    div();
+function gridItems (){
+    for (i = 0; i < (n * n); ++i) {
+    
+    createDiv();
+    };
 };
+gridItems();
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
+
+slider.oninput = function slide() {
+    n = this.value;
+    removeAllChildNodes(grid);
+    gridItems();
+    gridLayout();
+};
+
+//Stylistic changes to the grid: draw, erase, clear.      
 
 function draw(e){
-    ++clickCount;
-   
-    grid.addEventListener("mouseover", function(e){
         e.target.style.backgroundColor = '#000';
-    }, false);
-
 }; 
 
-function eraser(e){
-    grid.addEventListener("mouseover", function(e){
-        e.target.style.backgroundColor = '#fff';
-    }, false);
+function utensilDraw(){
 
-    if (grid.removeEventListener('click', eraser, false));
+    grid.addEventListener('click', function(){
+        clickCount++;
+
+        if (clickCount % 2 != 0){
+            grid.addEventListener('mouseover', draw, false);
+        }
+        
+        else if (clickCount % 2 == 0){
+            grid.removeEventListener('mouseover', draw, false);
+        };
+    });
 };
 
-function utensil(e){
+function utensil(){
+    
+    if (btnDraw.addEventListener('click', utensilDraw()));
 
-    if (btnDraw.addEventListener('click', function(e) {
-        if (grid.addEventListener('click', function(){
-            draw();
-            
-            if (clickCount > 1){
-                grid.removeEventListener('click', draw)
-            };
-            
-        }));
-    }));
+}
 
-    else if (btnEraser.addEventListener('click', function(e) {
-        if (grid.addEventListener('click', function(){
-            eraser();
-        }));
-    }));
-};
- 
 utensil();
-
-function clear(){
-    grid.style.backgroundColor = '#fff'
-};
-
-btnClear.onclick = clear;
